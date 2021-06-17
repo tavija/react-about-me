c349c8eb6a5ef8dc29d8274e731345cfe7b8f588
Tavija N <tavijaja@gmail.com>
2021-06-17 09:40:54 +0300

    Text edits in Header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,7 +2,7 @@ function PageHeader(): JSX.Element {
   return (
     <header>
       <h1>Ye Olde Web Page</h1>
-      <p>Welcome to Nichard's Ye Olde Web Page!</p>
+      <p>Welcome to Tavija's Ye Olde Web Page!</p>
       <p>
         This shows a random scatter of my thoughts, last updated on March 4,
         2003.
